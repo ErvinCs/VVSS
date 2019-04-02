935143516a1ba3c6581aa634af4de6d05a04f070
ErvinCs <csoka.ervin12@gmail.com>
2019-04-02 11:17:03 +0300

    Fixed a bug in StudentValidator

--- src/main/java/Validator/StudentValidator.java
+++ src/main/java/Validator/StudentValidator.java
@@ -16,8 +16,9 @@ public class StudentValidator implements Validator<Student> {
             m = m + "\nID invalid";
         if (st.getGrupa() < 111 || st.getGrupa() > 937 || st.getGrupa() % 100 / 10 < 1 || st.getGrupa() % 100 / 10 > 3 || st.getGrupa() % 10 < 1 || st.getGrupa() % 10 > 7)
             m = m + "\nGrupa invalida";
-        //TODO - The regex can still match invalid e-mails
-        if (!st.getMail().contains("@") || !st.getMail().contains("."))
+
+        if(!st.getMail().matches("[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*"))
+        //if (!st.getMail().contains("@") || !st.getMail().contains("."))
             m = m + "\nEmail invalid";
         if(!st.getNume().matches("[A-Za-z'-]+"))
             m=m+"\nNume invalid";
